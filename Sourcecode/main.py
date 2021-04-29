43066117191e008f70b2c72fa58246eb22baaace
lavanya-k19 <82080063+lavanya-k19@users.noreply.github.com>
2021-04-29 13:14:02 +0530

    Delete Sourcecode directory

--- Sourcecode/main.py
+++
@@ -1,6 +0,0 @@
-from donor import *
-A = blooddb("GBG", "saxaxi", "geethu", "Grce", "xjnsdkci")
-A.read_data(thelist)
-A.write_data(thelist)
-B = input("Enter phone num to search:")
-A.search_donor(B)
